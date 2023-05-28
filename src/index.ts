d0766ea9ae5db0981817d5f10c3cc5e1cfde512a
GiriMadhan <girimadhan7@gmail.com>
2023-05-28 23:36:07 +0530

    feat: added typescript function for replaceTokens.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 const arrays = require("./arrays");
 const numbers = require("./numbers");
+const strings = require("./strings");
 
 module.exports = {
   ...arrays,
   ...numbers,
+  ...strings,
 };
