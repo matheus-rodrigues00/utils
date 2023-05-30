8063f5a30c9b67430b3c45d7e816e0e959eddbf7
Gustavo Henrique <gustavo@n2solucoes.com.br>
2023-05-30 14:03:04 -0300

    ✅ Tests for the evaluate method
    This tests run all possibilities for this method

--- __tests__/strings.test.js
+++ __tests__/strings.test.js
@@ -1,4 +1,4 @@
-const { replaceTokens } = require("@/strings");
+const { replaceTokens, evaluate } = require("@/strings");
 
 describe("replaceTokens", () => {
   test("replaces tokens with values, using basic regex /(w+)/g", () => {
@@ -46,5 +46,40 @@ describe("replaceTokens", () => {
       regex
     );
     expect(result).toBe("This is a not string");
+  });
+});
+
+describe("evaluate", () => {
+  test("Receive two strings, '1 + 1' and return true", () => {
+    expect.assertions(1);
+    const result = evaluate("1 + 1");
+    expect(result).toBe(2);
+  });
+
+  // Tests for assert errors
+  test("Receive '1 plus 1' and return error", () => {
+    expect.assertions(1);
+    expect(() => {evaluate("1 plus 1")}).toThrowError('Unexpected identifier');
+  });
+
+  test("Receive '1 plus 1' and execute the callback function", () => {
+    expect.assertions(1);
+    const callback = ()=> {
+      throw new Error('Something went wrong');
+    };
+    expect(() => {evaluate("1 + 1", callback)}).not.toThrowError('Something went wrong');
+  });
+
+  test("Receive '1 plus 1' and execute the callback function with parameters", () => {
+    expect.assertions(1);
+    const num1 = 1;
+    const num2 = 9;
+
+    const callback = (first_num, second_num)=> {
+      const result = first_num + second_num;
+      expect(result).toBe(10);
+      return result;
+    };
+    evaluate("1 plus 1", callback(num1, num2));
   });
 });
