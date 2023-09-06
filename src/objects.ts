3ce2c4b39f255886ecf57b211482d69d0af484f3
Gustavo Henrique <ghboliveira@hotmail.com>
2023-09-06 17:23:32 +0000

    feat: implements the pick function

--- src/objects.ts
+++ src/objects.ts
@@ -6,5 +6,22 @@
 function deepClone(obj: object) {
     return { ...obj}
 }
-  
-export { deepClone };
+
+/**
+ * This method recieves an object and an array of keys and returns a new object with only the keys specified.
+ * @param source 
+ * @param keys 
+ * @returns {object}
+ */
+function pick(source: any, keys: string[]): object {
+  const result: any = {};
+  // we should also consider if the values are objects, if so we search recursively.
+  for (const key in source) {
+    if(keys.includes(key)){
+      result[key] = source[key]; 
+    }
+  }
+  return result;
+}
+
+export { deepClone, pick };
