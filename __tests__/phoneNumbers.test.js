63a53a96b40e2468ac881b08520e7819343dd54d
Matheus Rodrigues <joaoomatheus@hotmail.com>
2023-09-07 09:04:53 -0300

    test: added a few phoneNumbers tests

--- __tests__/phoneNumbers.test.js
+++ __tests__/phoneNumbers.test.js
@@ -1,13 +1,23 @@
 const { validBrazilianPhoneNumber } = require("@/phoneNumbers");
 
 describe("validBrazilianPhoneNumber", () => {
-  test("validate a BR phone is BR phone", () => {
+  test("validates that a brazilian phone is brazilian phone", () => {
     const result = validBrazilianPhoneNumber("(75) 9 9999-9999");
     expect(result).toBeTruthy();
   });
 
-  test("validate a incorret phone isnot BR phone", () => {
+  test("validates that invalid number size isn't valid brazilian", () => {
     const result = validBrazilianPhoneNumber("55 (75) 99 9999-9999");
     expect(result).toBeFalsy();
+  });
+
+  test("validates that empty string isn't valid brazilian", () => {
+    const result = validBrazilianPhoneNumber("");
+    expect(result).toBeFalsy();
+  });
+
+  test("validates that only numbers isn't valid brazilian", () => {
+    const result = validBrazilianPhoneNumber("75 9 9999 9999");
+    expect(result).toBeTruthy();
   });
 });
