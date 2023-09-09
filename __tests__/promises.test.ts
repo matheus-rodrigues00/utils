3fce090580021d2079d2898b293be13ed2f91d38
0min <ahn0min95@gmail.com>
2023-09-09 10:59:38 +0900

    feat: additional testcase for timeout method

--- __tests__/promises.test.ts
+++ __tests__/promises.test.ts
@@ -31,30 +31,29 @@ describe("sleep", () => {
   });
 });
 
-describe("timout", () => {
-  // 정상적일 때 체크할 데이터
+describe("timeout", () => {
   const data = "resolved data";
 
-  // 끝났고 요청성공일때
-  test("fulfilled within the specified time", async () => {
+  test("In case of not timeout and resolve", async () => {
     const success = await timeout(mockResolvedPromise(data, 100), 300);
     expect(success).toBe(data);
   });
 
-  // 끝났는데 요청실패일때
-  test("fulfilled within the specified time", async () => {
-    // response error인 경우
-    // const fail = await timeout(mockRejectedPromise(data, 100), 300);
-
+  test("In case of not timeout but rejected", async () => {
     await expect(async () => {
       await timeout(mockRejectedPromise(RESPONSE_ERROR_MESSAGE, 100), 300);
     }).rejects.toThrowError(new Error(RESPONSE_ERROR_MESSAGE));
   });
 
-  // 요청이 안왔는데 시간이 끝났을 때
-  test("not fulfilled within the specified time and new Timeout Error", async () => {
+  test("In case of timeout and resolve", async () => {
     await expect(async () => {
       await timeout(mockResolvedPromise(data, 300), 100);
+    }).rejects.toThrowError(new Error(TIMEOUT_ERROR_MESSAGE));
+  });
+
+  test("In case of timeout but rejected", async () => {
+    await expect(async () => {
+      await timeout(mockRejectedPromise(RESPONSE_ERROR_MESSAGE, 300), 100);
     }).rejects.toThrowError(new Error(TIMEOUT_ERROR_MESSAGE));
   });
 });
