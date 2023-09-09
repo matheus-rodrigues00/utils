efbb8e50c12a6ebb8603369f6e75d31b54c9bea5
0min <ahn0min95@gmail.com>
2023-09-09 10:22:17 +0900

    feat: add timeout method

--- src/promises.ts
+++ src/promises.ts
@@ -1,3 +1,6 @@
+const TIMEOUT_ERROR_MESSAGE = "Timeout Error";
+const RESPONSE_ERROR_MESSAGE = "Response Error";
+
 /**
  * This method receives a time in milliseconds and returns a promise that resolves after that time.
  * @param {number} time - The time in milliseconds.
@@ -7,4 +10,31 @@ function sleep(time: number = 1000) {
   return new Promise(resolve => setTimeout(resolve, time));
 }
 
-export { sleep };
+/**
+ * * If the promise is not fulfilled within the specified time, a Timeout Error is throw reject.
+ * @template T
+ * @param {Promise<T>} promise
+ * @param {number} [time=8000]
+ * @returns {Promise<T>}
+ * @throws {TimeoutError} Throws a TimeoutError if the timeout is exceeded.
+ */
+function timeout<T>(promise: Promise<T>, time = 8000) {
+  return new Promise((resolve, reject) => {
+    const timeoutId = setTimeout(
+      () => reject(new Error(TIMEOUT_ERROR_MESSAGE)),
+      time
+    );
+
+    promise
+      .then(response => {
+        clearTimeout(timeoutId);
+        resolve(response);
+      })
+      .catch(err => {
+        clearTimeout(timeoutId);
+        reject(err);
+      });
+  });
+}
+
+export { sleep, timeout, TIMEOUT_ERROR_MESSAGE, RESPONSE_ERROR_MESSAGE };
